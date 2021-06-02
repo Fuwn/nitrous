9733b9180b570de64153e01a6720d27cf8a5a5b3
Fuwn <fuwnzy@gmail.com>
2021-06-01 19:24:21 -0700

    refactor(clippy): strictor clippy constraints

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 // Copyleft (ɔ) 2021-2021 Fuwn
 // SPDX-License-Identifier: GPL-3.0-only
 
-#![warn(rust_2018_idioms)]
+#![feature(
+  type_ascription,
+  hash_set_entry,
+  type_name_of_val,
+  decl_macro,
+  proc_macro_hygiene
+)]
+#![deny(
+  warnings,
+  nonstandard_style,
+  unused,
+  future_incompatible,
+  rust_2018_idioms,
+  unsafe_code
+)]
+#![deny(clippy::all, clippy::nursery, clippy::pedantic)]
 #![recursion_limit = "128"]
 
 #[macro_use]
