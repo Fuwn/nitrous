bfc19931935dfcaa7f849982e6097ef7f21b143d
Fuwn <contact@fuwn.me>
2021-06-07 01:31:32 -0700

    ci: create benchmark

---
+++ benches/bench.rs
@@ -0,0 +1,21 @@
+// Copyleft (ɔ) 2021-2021 Fuwn
+// SPDX-License-Identifier: GPL-3.0-only
+
+#[macro_use]
+extern crate criterion;
+
+use rand::Rng;
+use criterion::Criterion;
+
+fn bench_generate(c: &mut Criterion) {
+  c.bench_function("generate", |b| b.iter(|| {
+    rand::thread_rng()
+      .sample_iter(rand::distributions::Alphanumeric)
+      .take(16)
+      .map(char::from)
+      .collect::<String>()
+  }));
+}
+
+criterion_group!(benches, bench_generate);
+criterion_main!(benches);
